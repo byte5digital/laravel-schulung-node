e66fd22245b112ed6821a5c6e3172a82d944d863
Marcel Wege <mwege@byte5.de>
2021-06-23 13:45:33 +0200

    feat: add json parsing

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import * as express from 'express'
-import {request} from "express";
 
 export const app = express()
 
-app.get('/', async(req, res)=>{
-	res.json({
-		message: "Hallo an Alle"
-	}).end()
+app.use(express.json())
+
+app.post('/', (req, res)=>{
+	res.json(req?.body ?? {})
+	res.end()
 })
 
 const PORT = 3333
